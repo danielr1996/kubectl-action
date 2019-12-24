e7a7b097ea093a5a2b1656351c155ea0dd4cf880
Daniel Richter <danielrichter@posteo.de>
2019-12-24 16:42:37 +0100

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,9 @@
 
 set -e
 
-echo #########################
-echo # Github kubectl Action #
-echo #########################
-
 # Extract the base64 encoded config data and write this to the KUBECONFIG
 mkdir -p ~/.kube
 echo $INPUT_KUBECONFIG | base64 -d > ~/.kube/config
-cat deployment.yaml
-# Deploy deployment.yaml
+
+# Execute kubectl command
 sh -c "kubectl $*"
